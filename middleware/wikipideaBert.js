ae20410735967c0c7ddffbc94c5a57fa91d7aba0
Fatenkh7 <khodorfaten5@gmail.com>
2023-05-13 02:05:47 +0300

    doneee

--- middleware/wikipideaBert.js
+++ middleware/wikipideaBert.js
@@ -25,14 +25,14 @@ async function getWikipediaAnswer(question) {
   // Construct the Wikipedia API query URL
   const encodedQuestion = encodeURIComponent(question.trim());
   const apiUrl = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&srsearch=${encodedQuestion}&utf8=`;
-  
+
   try {
     const response = await axios.get(apiUrl);
     const pages = response.data.query.search;
     const page = pages[0];
     const content = page.snippet;
-    console.log(content);
-  
+    console.log("content", content);
+
     // Use the QnA model to answer the question
     const answers = await qnaModel.findAnswers(question, content);
     console.log("response.data", content);
@@ -40,19 +40,18 @@ async function getWikipediaAnswer(question) {
       console.log(`No answers found for '${question}'.`);
       return { answer: null, score: null };
     }
-  
+
     // Return the best answer and its score
     const answer = { answer: answers[0].text, score: answers[0].score };
-  
+
     // Cache the answer for future use
     cache.set(question, answer);
-  
+    console.log(answer);
     return answer;
   } catch (error) {
     console.error(`Error fetching Wikipedia page: ${error}`);
     return { answer: null, score: null };
   }
-  
 }
 
 export default getWikipediaAnswer;
